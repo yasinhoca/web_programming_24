38d84e2640bfb6fa092fb3f66a21bcf9edfaa8b6
Yasin Hoca <myasinozsaglam@gmail.com>
2024-03-15 18:20:21 +0300

    Add files via upload

---
+++ 12_php_giris.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Giriş</title>
+    <style>
+        #t{
+            font-size: 20px;
+            border: 2px solid red;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+echo "<h2> PHP'ye hoşgeldiniz!</h2>";
+echo "<input type='text' id='t'>"
+?>
+
+
+</body>
+</html>
