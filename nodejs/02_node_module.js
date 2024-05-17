e343854ab8e951f68b406ca8e58e42f3d23c12b2
Yasin Hoca <myasinozsaglam@gmail.com>
2024-05-17 16:57:24 +0300

    Add files via upload

---
+++ nodejs/02_node_module.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var tarih = require('./tarihmodule');
+
+
+
+
+
+
+http.createServer(function (req,res){
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'} );
+    res.write("Sistemin tarih ve saati : " + tarih.myDatetime());
+    res.end();
+}).listen(8080);
---
+++ nodejs/strmodule.js
@@ -0,0 +1,8 @@
+exports.myStr = function (){
+    var html = "<h1>Ben modülden gelen başlığım</h1>" +
+        "<br><br>" +
+        "<form>" +
+        "<input type='text' name='kullaniciAdi'>" +
+        "<input type='submit' value='Modul' style='font-size: 24'></form>";
+    return html;
+}
